af7ffb6b100974b9de2a20727b7f2009caf5298e
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-07-28 22:56:32 +0530

    firebase added

--- app/src/main/java/com/ayushidoshi56/scribble/MainActivity.kt
+++ app/src/main/java/com/ayushidoshi56/scribble/MainActivity.kt
@@ -2,14 +2,44 @@ package com.ayushidoshi56.scribble
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    private lateinit var postReference: DatabaseReference
+    private lateinit var childEventListener: ChildEventListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val paintview=PaintView(this)
         setContentView(paintview)
+        database= FirebaseDatabase.getInstance().reference
+        postReference = database.child("data")
+        childEventListener = object : ChildEventListener{
+
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // not needed
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                // not needed
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                // not needed
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                // not needed
+            }
+
+        }
 //        paintview.co(100f,105f)
 //        paintview.co(201f,701f)
 //        paintview.co(302f,603f)
--- app/src/main/java/com/ayushidoshi56/scribble/PaintView.kt
+++ app/src/main/java/com/ayushidoshi56/scribble/PaintView.kt
@@ -12,14 +12,18 @@ import android.view.MotionEvent
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.LinearLayout
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import android.view.View as View1
+
 
 var brush = Paint()
 var path = Path()
 lateinit var params:LinearLayout.LayoutParams
 
 class PaintView(context: Context?): android.view.View(context) {
-
+    private lateinit var database: FirebaseDatabase
+    private lateinit var postReference: DatabaseReference
     init{
         brush.isAntiAlias =true
         brush.setColor(Color.BLACK)
@@ -29,27 +33,41 @@ class PaintView(context: Context?): android.view.View(context) {
 
         params= LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
     }
-    public fun co(x:Float,y:Float)
+    public fun start(x:Float,y:Float)
     {
         path.moveTo(x, y);
+    }
+    public fun co(x:Float,y:Float)
+    {
         path.lineTo(x+4, y+4)
     }
     override fun onTouchEvent(event: MotionEvent): Boolean {
         var pointX = event.x
         var pointY = event.y
+        var data= mutableListOf<Float>()
+        data.add(pointX)
+        data.add(pointY)
+        database = FirebaseDatabase.getInstance()
+        postReference=database.reference
 
         when(event.action)
         {
             MotionEvent.ACTION_DOWN -> {
                 path.moveTo(pointX, pointY);
+                data.add(0f)
+                postReference.child("data").push().setValue(data)
                 return true
             }
             MotionEvent.ACTION_UP -> {
                 path.moveTo(pointX, pointY);
+                data.add(1f)
+                postReference.child("data").push().setValue(data)
                 return true
             }
             MotionEvent.ACTION_MOVE -> {
                 path.lineTo(pointX, pointY)
+                data.add(2f)
+                postReference.child("data").push().setValue(data)
             }
             else -> return false
         }
